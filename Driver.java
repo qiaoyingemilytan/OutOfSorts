8caa904452fd643fb99adcff91dc3db13482193f
Emily <>
2020-12-22 13:04:48 -0500

    random array

--- Driver.java
+++ Driver.java
@@ -12,7 +12,9 @@ public class Driver{
 
       //overhead: 1 random array generation.
       if(args.length < 3 || args[2].equals("random")){
-	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
+	      for(int i = 0; i < randData.length; i++){
+          randData[i] = (int)(Math.random() * 100);
+        }
       }else if(args[2].equals("equal")){
 	      System.out.println("REPLACE THIS LINE WITH ARRAY VALUE GENERATION!");
       }else if(args[2].equals("sorted")){
